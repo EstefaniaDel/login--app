c76332cd4d8e705fd4fefb72db64f7eb716b0558
Estefania <64932013+EstefaniaDel@users.noreply.github.com>
2023-09-13 15:38:25 -0300

    feat: frontend initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="vh-100 gradient-custom">
+      <div className="container">
+        <h1 className="page-header text-center">
+          {/* {" "} */}
+          React and Python Login Register
+        </h1>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
