28cf8306e8ec0ad827240fb7c96ec867848841f5
Estefania <64932013+EstefaniaDel@users.noreply.github.com>
2023-09-15 12:14:32 -0300

    fix: remove unnecessary code

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,64 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div className="container h-100 d-flex justify-content-center align-items-center">
-        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-          <form>
-            <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-              <p className="lead fw-normal mb-0 me-3">Log Into Your Account</p>
-            </div>
-
-            <div className="form-outline form-white mb-4">
-              <input
-                type="email"
-                id="form3example3"
-                className="form-control form-control-lg"
-                placeholder="Enter a valid email address"
-              />
-            </div>
-            <div className="form-outline mb-3">
-              <input
-                type="password"
-                id="form3Example4"
-                className="form-control form-control-lg"
-                placeholder="Enter password"
-              />
-              <label className="form-label" for="form3Example4">
-                Password
-              </label>
-            </div>
-
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="form-check mb-0">
-                <input
-                  className="form-check-input me-2"
-                  type="checkbox"
-                  value=""
-                  id="form2Example3"
-                />
-                <label className="form-check-label" for="form2Example3">
-                  Remember me
-                </label>
-              </div>
-              <a href="#!" className="text-body">
-                Forgot password?
-              </a>
-            </div>
-
-            <div className="text-center text-lg-start mt-4 pt-2">
-              <button type="button" className="btn btn-primary btn-lg">
-                Login
-              </button>
-              <p className="small fw-bold mt-2 pt-1 mb-0">
-                Don't have an account?{" "}
-                <a href="/register" className="link-danger">
-                  Register
-                </a>
-              </p>
-            </div>
-          </form>
+    <div className="container h-100">
+      <div className="row h-100 d-flex justify-content-center align-items-center">
+        <div className="col-12 text-center">
+          <h2 className="text-secondary">Welcome to this React Application</h2>
+          <p>
+            <Link to="/login" className="btn btn-success">
+              Login
+            </Link>{" "}
+            |{" "}
+            <Link to="/register" className="btn btn-success">
+              Register
+            </Link>{" "}
+          </p>
         </div>
       </div>
     </div>
