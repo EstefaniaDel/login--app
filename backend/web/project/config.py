37ffe49ff69b12f4d81bf6e80072d666b2c63045
Estefania <64932013+EstefaniaDel@users.noreply.github.com>
2023-09-15 01:51:53 -0300

    fix: url

--- backend/web/project/config.py
+++ backend/web/project/config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:secret@localhost:5432/postgres")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
