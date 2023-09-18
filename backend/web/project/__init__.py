f6743495f3cd669a084effbd68e665a2f064b7ad
Estefania <64932013+EstefaniaDel@users.noreply.github.com>
2023-09-18 16:05:33 -0300

    feat: add chaanges for auth

--- backend/web/project/__init__.py
+++ backend/web/project/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, make_response, request, session
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-
+from flask_login import LoginManager
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
 
 app = Flask(__name__)
 
@@ -10,6 +11,14 @@ app.config.from_object('project.config.Config')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 CORS(app, supports_credentials=True,  origins="http://localhost:3000")
+app.config["JWT_SECRET_KEY"] = "SECRET_KEY"
+app.config['JWT_TOKEN_LOCATION'] = ['headers']
+jwt = JWTManager(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
 
 
 class User (db.Model):
@@ -24,6 +33,9 @@ class User (db.Model):
         self.email = email
         self.password = password
 
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+
 
 @app.route('/login', methods=['POST'])
 def login_user():
@@ -31,19 +43,23 @@ def login_user():
     email = data.get('email')
     password = data.get('password')
 
+    print(f"email: {email}, password: {password}")
+
     user = User.query.filter_by(email=email).first()
 
     if user is None:
         return jsonify({'message': 'Invalid email or password'}), 401
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({'message': 'Invalid email or password'}), 401
-    return jsonify({'message': 'Login successful'}), 200
+    access_token = create_access_token(identity=user.id)
+    ret = {'access_token': access_token, 'id':  user.id,
+           'email': user.email, 'username': user.username}
+    return jsonify(ret), 200
 
 
 @app.route('/register', methods=['POST'])
 def create_user():
     try:
-
         data = request.get_json()
         new_user = User(
             username=data['username'],
@@ -57,12 +73,6 @@ def create_user():
         return jsonify({'message': 'User created'}), 201
     except Exception as e:
         return jsonify({'message': 'Error creating user'}), 500
-
-
-@app.route('/logout', methods=['GET'])
-def logout_user():
-    logout_user()
-    return jsonify({'message': 'Logout successful'}), 200
 
 
 @app.route('/users', methods=['GET'])
@@ -96,14 +106,16 @@ def update_user(id):
         user = User.query.get(id)
         if user:
             data = request.get_json()
-            user.username = data['username']
-            user.email = data['email']
+            if data.get('username'):
+                user.username = data['username']
+            if data.get('email'):
+                user.email = data['email']
             db.session.commit()
             return jsonify({'message': 'User updated'}), 200
         else:
             return jsonify({'message': 'User not found'}), 404
     except Exception as e:
-        return jsonify({'message': 'Error updating user'}), 500
+        return jsonify({'message': 'Error updating user', e: e}), 500
 
 
 @app.route('/users/<int:id>', methods=['DELETE'])
@@ -113,11 +125,17 @@ def delete_user(id):
         if user:
             db.session.delete(user)
             db.session.commit()
-            return jsonify({'message': 'User deleted'}), 200
+            return jsonify({'message': 'User account deleted'}), 200
         else:
             return jsonify({'message': 'User not found'}), 404
     except Exception as e:
-        return jsonify({'message': 'Error deleting user'}), 500
+        return jsonify({'message': 'Error deleting user account'}), 500
+
+
+@app.route('/protected', methods=['GET'])
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 
 @app.route('/', methods=['GET'])
