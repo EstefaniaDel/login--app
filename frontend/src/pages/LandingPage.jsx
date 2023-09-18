0dd3408ac4a9f11b726c871435c0c054262db295
Estefania <64932013+EstefaniaDel@users.noreply.github.com>
2023-09-18 16:10:28 -0300

    feat: change title

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
     <div className="container h-100">
       <div className="row h-100 d-flex justify-content-center align-items-center">
         <div className="col-12 text-center">
-          <h2 className="text-secondary">Welcome to this React Application</h2>
+          <h2 className="text-secondary">Welcome to this Flask-React Application</h2>
           <p>
             <Link to="/login" className="btn btn-success">
               Login
